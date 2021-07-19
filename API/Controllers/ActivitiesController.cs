5359986cefa1d6583d3c8f71d5d0bf1d93b9a549
Dmytrocode <dmytrocontact@gmail.com>
2021-07-19 16:56:17 -0600

    Setting up controllers API

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,15 @@
+using Persistence;
+
+namespace API.Controllers
+{
+
+
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
