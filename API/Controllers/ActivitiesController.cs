0e606bb72cb5a8b2d5a76b83aea3425f596c57f7
Dmytrocode <dmytrocontact@gmail.com>
2021-07-19 22:48:19 -0600

    Add React.js to my Application

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
@@ -10,6 +16,17 @@ namespace API.Controllers
         public ActivitiesController(DataContext context)
         {
             _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")] //activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+
         }
     }
 }
